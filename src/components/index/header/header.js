d8d253ccd6cf85920d05a14166f2524669e1356e
Facundo Merighi <merighifacundo@gmail.com>
2018-11-30 03:12:26 -0500

    Adding connector modal.

--- src/components/index/header/header.js
+++ src/components/index/header/header.js
@@ -6,65 +6,55 @@ class Header extends React.Component {
     constructor(props) {
         super(props);
     }
-    render( ) {
-        return (<header>
-    
-        <div className="fixed-navbar">
-    
-            <div className="navbar-lockup">
-    
-                
-    
-                <ul className="nav">
-    
-                    <li><a href="">Contribute</a></li>
-                    <li><a href="">Frontend Docs</a></li>
-                    <li><a href="">Api Docs</a></li>
-                    <li><a href="">Support</a></li>
 
+    handleClick() {
+        alert("Click me");
+    }
+
+    render( ) {
+        return (
+        <header>
+            <div className="fixed-navbar">
+                <div className="navbar-lockup">
+                    <ul className="nav">        
+                        <li><a href="">Contribute</a></li>
+                        <li><a href="">Frontend Docs</a></li>
+                        <li><a href="">Api Docs</a></li>
+                        <li><a href="#modal" onClick={this.handleClick}>Start Here</a></li>
+                    </ul>        
+                </div>
+            </div>
+            <div className="claim-lockup">
+                <div className="claim">
+                    <p className="headline">MyRunHub.com</p>
+                    <p className="kicker">Open Source solution</p>
+                    <p className="kicker">for Fitness Data.</p>
+                </div>
+            </div>
+            <div className="angled-overlay"></div>
     
+            <div className="slide-out-nav">
+        
+                <ul className="mobile-nav">
+        
+                <li><a href="">Contribute</a></li>
+                <li><a href="">Frontend Docs</a></li>
+                <li><a href="">Api Docs</a></li>
+                <li><a href="">Start Here</a></li>
+        
                 </ul>
-    
+        
+                <div className="mobile-nav-slice"></div>
+        
             </div>
-    
-        </div>
-    
-        <div className="claim-lockup">
-    
-            <div className="claim">
-    
-                <p className="headline">MyRunHub.com</p>
-                <p className="kicker">Open Source solution</p>
-                <p className="kicker">for Fitness Data.</p>
-    
+        
+            <div className="mobile-nav-toggle">
+        
+                <span></span>
+                <span></span>
+                <span></span>
+        
             </div>
-    
-        </div>
-    
-        <div className="angled-overlay"></div>
-    
-        <div className="slide-out-nav">
-    
-            <ul className="mobile-nav">
-    
-            <li><a href="">Contribute</a></li>
-            <li><a href="">Frontend Docs</a></li>
-            <li><a href="">Api Docs</a></li>
-            <li><a href="">Support</a></li>
-    
-            </ul>
-    
-            <div className="mobile-nav-slice"></div>
-    
-        </div>
-    
-        <div className="mobile-nav-toggle">
-    
-            <span></span>
-            <span></span>
-            <span></span>
-    
-        </div>
     
     </header>);
     }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./components/index/header/header.js";
 import Footer from "./components/index/footer/footer.js";
+import Modal from "./components/index/logon/modal.js";
 import './index.css';
 
 class Application extends React.Component {
     render() {
         return (
         <div>
+        <Modal></Modal>
         <Header></Header>
         <Footer></Footer>
         </div>
